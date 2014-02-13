cfcbeae1b8d027e0303ac8c2186196aff1b65ed5
Simon Hardijanto <metaluna4@hellokitty.com>
2014-02-13 10:27:05 +0000

    Initial commit

---
+++ app/views/stories/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @story, :id, :title, :abstract, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boken::Application.config.secret_key_base = '2fd17d402fb0ddddce1274d99678ef6652bee7b90afa913d96ffbe1f345193ee7b6fa1821c5938926e5104c2ecb86a8f6cbf7a8162be9dfdc2f92dde4223ba96'
