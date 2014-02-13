024c40b031bceb78364f8a79f3c0060fb7184e05
Simon Hardijanto <metaluna4@hellokitty.com>
2014-02-13 15:40:29 +0000

    + integration test for Create Story

---
+++ features/story/create_story.feature
@@ -0,0 +1,4 @@
+Feature: Create story
+   Scenario: List new story on homepage
+      When I create a new story
+      Then the story is listed on the homepage
---
+++ features/story/step_definitions/story_steps.rb
@@ -0,0 +1,11 @@
+When(/^I create a new story$/) do
+  @story = FactoryGirl.build(:story_with_abstract)
+  visit '/stories/new'
+  fill_in 'Title', with: @story.title
+  fill_in 'Abstract', with: @story.abstract
+end
+
+Then(/^the story is listed on the homepage$/) do
+  expect(page).to have_content @story.title
+  expect(page).to have_content @story.abstract
+end
